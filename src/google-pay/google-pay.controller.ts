dcbfc2e4e9076ff1680cd48752235930dd4b2edb
sharan_personal <shoronshark@gmail.com>
2025-10-21 17:45:06 +0600

    feat(google-pay): demo endpoint created

--- src/google-pay/google-pay.controller.ts
+++ src/google-pay/google-pay.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GooglePayService } from './google-pay.service';
 
 @Controller('google-pay')
-export class GooglePayController {}
+export class GooglePayController {
+    constructor(private readonly paymentService: GooglePayService) { }
+
+    @Post('make-payment')
+    async handleGooglePay(@Body() body: any) {
+        const { orderId, amount, paymentToken } = body;
+        return this.paymentService.handleGooglePayPayment(orderId, amount, paymentToken);
+    }
+
+}
